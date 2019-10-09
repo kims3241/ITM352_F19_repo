2f370c54c9d425c9c4acb4060cf62de507574409
kims3241 <54648545+kims3241@users.noreply.github.com>
2019-10-09 13:02:02 -1000

    Create Ex4.js

---
+++ Lab4/Ex4.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+    console.log(req.headers); //output the request headers to the console
+    res.writeHead(200, { 'Content-Type': 'text/html' }); // set MIME type to HTML 
+    res.write('<META http- equiv ="refresh" content="0;URL=http://www.google.com">');
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+console.log('Hello world HTTP server listening on localhost port 8080');
