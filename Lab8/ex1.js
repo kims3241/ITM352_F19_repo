5ea915e8945c53cc87703a0986850674c0f9e393
kims3241 <54648545+kims3241@users.noreply.github.com>
2019-10-09 13:01:26 -1000

    Create ex1.js

---
+++ Lab8/ex1.js
@@ -0,0 +1,22 @@
+var age = 27;
+var counters = 1;
+// repeat until counters equals age 
+while (counters != age) {
+    console.log('counter; ' + counters);
+     counters++
+}
+
+//var age = 27;
+//var counters = 0;
+//repeat until counters equals age 
+//while (counters++ != age) {
+    //console.log('counter; ' + counters);
+//}
+
+//var age = 27;
+//var counters = 0;
+//repeat until counters equals age 
+//while (counters <= age) {
+    //console.log('counter; ' + counters);
+    //counters++
+//}
